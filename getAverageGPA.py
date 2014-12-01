4923a346c4bc89fd5b166af25505c0fa17eb697a
gallonp <gallonpig@gmail.com>
2014-11-30 19:02:01 -0800

    adapt to support new data set

--- getAverageGPA.py
+++ getAverageGPA.py
@@ -1,18 +1,29 @@
 import pandas as pd
 
 
-csv = pd.read_csv('cs_data_year.csv')
+csv = pd.read_csv('CourseDatav3.csv')
 courseKey = set(csv['Course'].tolist())
 output = pd.DataFrame(columns=['Course','Year','GPA'])
+tys = csv['Year'].tolist()
+
+yearss = []
+for ty in tys:
+    t,y = ty.split()
+    yearss.append(y)
+    
+csv['Y'] = yearss
 for course in courseKey:
     c = csv[csv['Course']==course]
-    years = set(csv['Year'].tolist())
+    years = set(c['Y'].tolist())
+    years = list(years)
+    years.sort()
+    # print years
     for year in years:
-        cy = c[(c['Year']==year) &(c['GPA']!=0)]
+        cy = c[(c['Y']==year) &(c['GPA']!=0)]
         if len(cy) == 0: continue
         gpaAverage  = sum(cy['GPA'].tolist())/float(len(cy))    
         row = [{'Course':course, 'Year':year, 'GPA':gpaAverage}]
         output = output.append(row)
 
 
-output.to_csv('output.csv',index=None)
+output.to_csv('output3.csv',index=None)
